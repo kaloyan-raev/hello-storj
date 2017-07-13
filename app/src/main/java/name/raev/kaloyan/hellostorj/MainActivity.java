ca151f392a977130aa3f4ca24ef038f503908d56
Kaloyan Raev <kaloyan@raev.name>
2017-07-13 16:38:21 +0300

    Remove floating action button

--- app/src/main/java/name/raev/kaloyan/hellostorj/MainActivity.java
+++ app/src/main/java/name/raev/kaloyan/hellostorj/MainActivity.java
@@ -24,15 +24,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
